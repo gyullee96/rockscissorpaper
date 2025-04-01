091f1ef0ee59f72bb3d0df0c1601cb87840b04e9
gyullee96 <gyullee96@naver.com>
2025-04-01 22:40:45 +0900

    third commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import './App.css';
 import Box from './component/Box'
 
@@ -8,17 +9,78 @@ import Box from './component/Box'
 // 5. 승패를 가지고 승자와 패자를 나눈다.
 // 6. 승패결과에 따라 테두리 색이 바뀐다. (승-초, 패-빨, 무-검)
 
+const choice = {
+  rock: {
+    name: "Rock",
+    img: "https://images.unsplash.com/photo-1584630932270-fe617b089c02?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmlzdHxlbnwwfHwwfHx8MA%3D%3D"
+  },
+  scissors: {
+    name: "Scissors",
+    img: "https://images.unsplash.com/photo-1611075384257-16ab912e8228?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fHZpY3RvcnklMjBzaWdufGVufDB8fDB8fHww"
+  },
+  paper: {
+    name: "Paper",
+    img: "https://images.unsplash.com/photo-1545166867-476da315406e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTc5fHxoYW5kfGVufDB8fDB8fHww"
+  }
+};
+
 function App() {
+  const [userSelect, setUserSelect] = useState(null)
+  const [comSelect, setComSelect] = useState(null)
+  const [userResult, setUserResult] = useState("")
+  const [comResult, setComResult] = useState("")
+  const play = (userChoice) => {
+    setUserSelect(choice[userChoice]);
+    let computerChoice = randomChoice();
+    setComSelect(computerChoice);
+    setUserResult(judgement1(choice[userChoice], computerChoice));
+    setComResult(judgement2(computerChoice, choice[userChoice]));
+  };
+
+  const randomChoice = () => {
+    let itemArray = Object.keys(choice);
+    console.log("item array: ", itemArray);
+    let randomItem = Math.floor(Math.random() * itemArray.length);
+    let final = itemArray[randomItem];
+    return choice[final];
+  };
+
+  const judgement1 = (user, computer) => {
+    console.log("user", user, "computer", computer);
+
+    if (user.name === computer.name) {
+      return "Tie";
+    } else if (user.name === "Rock") {
+      return computer.name === "Scissors" ? "Win" : "Lose"
+    } else if (user.name === "Scissors") {
+      return computer.name === "Paper" ? "Win" : "Lose"
+    } else if (user.name === "Paper") {
+      return computer.name === "Rock" ? "Win" : "Lose"
+    }
+  };
+
+  const judgement2 = (computer, user) => {
+    if (computer.name === user.name) {
+      return "Tie";
+    } else if (computer.name === "Rock") {
+      return user.name === "Scissors" ? "Win" : "Lose"
+    } else if (computer.name === "Scissors") {
+      return user.name === "Paper" ? "Win" : "Lose"
+    } else if (computer.name === "Paper") {
+      return user.name === "Rock" ? "Win" : "Lose"
+    }
+  }
+
   return (
     <div>
       <div className='main'>
-        <Box title="You" />
-        <Box title="Computer" />
+        <Box title="You" item={userSelect} result={userResult} />
+        <Box title="Computer" item={comSelect} result={comResult} />
       </div>
       <div className="main">
-        <button>가위</button>
-        <button>바위</button>
-        <button>보</button>
+        <button onClick={() => play("scissors")}>가위</button>
+        <button onClick={() => play("rock")}>바위</button>
+        <button onClick={() => play("paper")}>보</button>
       </div>
     </div>
   );
--- src/component/Box.js
+++ src/component/Box.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Box = (props) => {
     return (
-        <div className='box'>
+        <div className={`Box ${props.result}`}>
             <h1>{props.title}</h1>
-            <img src='https://images.unsplash.com/photo-1584630932270-fe617b089c02?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8ZmlzdHxlbnwwfHwwfHx8MA%3D%3D' className='img'></img>
-            <h2>WIN</h2>
+            <img className="item-img" src={props.item && props.item.img}></img>
+            <h2>{props.result}</h2>
         </div>
     )
 }
